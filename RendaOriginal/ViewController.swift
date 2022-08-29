0faa20d16875cf3b398e02f33920a05fd2278e6d
KeitaroKawahara <keitaro.inc@gmail.com>
2022-08-29 13:57:39 +0900

    Initial Commit

---
+++ RendaOriginal/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RendaOriginal
+//
+//  Created by Keitaro Kawahara on 2022/08/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
